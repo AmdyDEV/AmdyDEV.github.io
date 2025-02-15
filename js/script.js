48b5cdd4b3278150685abdd355da6a45d4d6ab48
Mouhamadou Moustapha NDIAYE <peuzziboe0502@gmail.com>
2025-02-15 16:31:20 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// ============================ typing animation================
+var typed = new Typed(".typing",{
+    strings:["","Developpeur Web","web Designer","Graphic Designer","Youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+}
+
+)
